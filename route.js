4d5d2784f6fc70f8dbc72d2c1f673c5b4b638828
MagicaQuartet <dev.dskim@gmail.com>
2018-11-24 13:17:24 +0900

    update existing review

--- route.js
+++ route.js
@@ -23,35 +23,53 @@ router.get('/recommendation', function (req, res) {
 });
 
 router.get('/listReview', function (req, res) {
-	connection.query('SELECT * FROM user_review WHERE name LIKE \'%'+req.query.name+'%\'ORDER BY rating DESC', function (err, rows) {
+	connection.query('SELECT * FROM user_review WHERE name LIKE \'%'+req.query.name+'%\'ORDER BY rating DESC;', function (err, rows) {
 		if (err) throw err;
 		res.send(rows);
 	})
 });
 
 router.get('/listGame', function (req, res) {
-	connection.query('SELECT * FROM all_games WHERE name LIKE \'%'+req.query.name+'%\'', function (err, rows) {
+	connection.query('SELECT * FROM all_games WHERE name LIKE \'%'+req.query.name+'%\';', function (err, rows) {
 		if (err) throw err;
 		res.send(rows);
 	})
 });
 
 router.post('/writeReview', function (req, res) {
-	selectQuery = 'SELECT id FROM all_games WHERE all_games.name="'+req.body.name+'";';
+	var duplicate = false;
+
+	connection.query('SELECT * FROM user_review WHERE name=\''+req.body.name+'\';', function (err, rows) {
+		if (err) throw err;
+		if (rows.length > 0)
+			duplicate = true;
+	})
+
+	selectQuery = 'SELECT id FROM all_games WHERE all_games.name=\''+req.body.name+'\';';
 
 	connection.query(selectQuery, function (err, row) {
 		if (err) throw err;
 		console.log(row);
 		
-		insertQuery = 'INSERT INTO user_review (gid, name, rating) VALUES ('
-						+ row[0].id + ', "'
-						+ req.body.name + '", '
-						+ req.body.rating + ');';
+		if (duplicate) {
+			updateQuery = 'UPDATE user_review SET rating='+req.body.rating+' WHERE name=\''+req.body.name+'\';'
 
-		connection.query(insertQuery, function (err, rows) {
-			if (err) throw err;
-			console.log(rows);
-		});
+			connection.query(updateQuery, function (err, rows) {
+				if (err) throw err;
+				console.log(rows);
+			})
+		}
+		else {
+			insertQuery = 'INSERT INTO user_review (gid, name, rating) VALUES ('
+							+ row[0].id + ', "'
+							+ req.body.name + '", '
+							+ req.body.rating + ');';
+
+			connection.query(insertQuery, function (err, rows) {
+				if (err) throw err;
+				console.log(rows);
+			});
+		}
 	});
 });
 
