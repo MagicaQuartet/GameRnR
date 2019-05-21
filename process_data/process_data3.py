fc54b728a5f62fb08add72a3a0d3e1f8377d0fca
MagicaQuartet <dev.dskim@gmail.com>
2019-05-21 14:12:27 +0000

    Update .gitignore

--- process_data/process_data3.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for file in os.listdir("./tmp_data2/"):
-    with open("./tmp_data2/"+file, "r") as input_file, open("./data/"+file, "w") as output_file:
-        for line in input_file:
-            print(line.rstrip().split(","))
-    break
